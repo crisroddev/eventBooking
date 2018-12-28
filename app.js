4447859549ab2e43689bb0cbf2c9add60941c1cd
crodriguez85 <cristianrodriguez@Cristians-MacBook-Air.local>
2018-12-28 18:20:58 -0300

    Setting Up graphQl eniroment

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+//{Estan las propiedades de los schemas}
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 // Parse JSON bodys
 app.use(bodyParser.json());
 
-//Parametros: req(request), res(response), next
-app.get('/', (req, res, next) => {
-    res.send('Hello Wold!!!');
-})
+// //Parametros: req(request), res(response), next. Coemntado por que asi se hace en express
+// app.get('/', (req, res, next) => {
+//     res.send('Hello Wold!!!');
+// })
 
-//Port Listen
-app.listen(3000)
+// GraphQL endpoint donde se manda todo
+app.use(
+    '/graphql',
+    graphqlHttp({
+    //Confuguro graphql API, donde esta el schema, querys, resolvers
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
+
+app.listen(3000);
