7cc076174064003d423c34f64d72976d0bfb5b80
crodriguez85 <cristianrodriguez@Cristians-MacBook-Air.local>
2018-12-31 14:19:30 -0300

    MongoDB set up

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-//{Estan las propiedades de los schemas}
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
 
-// Parse JSON bodys
 app.use(bodyParser.json());
 
-// //Parametros: req(request), res(response), next. Coemntado por que asi se hace en express
-// app.get('/', (req, res, next) => {
-//     res.send('Hello Wold!!!');
-// })
-
-// GraphQL endpoint donde se manda todo
 app.use(
-    '/graphql',
-    graphqlHttp({
-    //Confuguro graphql API, donde esta el schema, querys, resolvers
+  '/graphql',
+  graphqlHttp({
     schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
         schema {
             query: RootQuery
@@ -33,15 +40,46 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
-      createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       }
     },
+
     graphiql: true
   })
 );
 
-app.listen(3000);
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@booking-dev-xhqj5.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
+    {useNewUrlParser: true})
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
